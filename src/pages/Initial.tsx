8f8c66838a302104f497f3adb39219a4056fb5cd
Kobus <jacobusbad@gmail.com>
2023-05-18 00:45:12 -0600

    does terraform work?

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -3,6 +3,6 @@ import * as React from 'react'
 
 export const Initial: React.FC = () => {
     return (
-        <h1>Initial page. Bonjourno</h1>
+        <h1>Initial page. Bonjourno TERRAFORM WORKS.</h1>
         );
 }
