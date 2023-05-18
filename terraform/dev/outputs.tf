c25ee6a093d1abdd370d671a5a5f92ef343a41d9
Kobus <jacobusbad@gmail.com>
2023-05-18 00:09:47 -0600

    circleci integration

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -1,13 +1,14 @@
 output "bucket_id" {
   description = "Bucket ID for s3 holding out website files"
-  value = aws_s3_bucket.web_bucket.id
+  value       = module.dev.bucket_id
 }
+
 output "cloudfront_domain_name" {
   description = "Cloudfront domain name"
-  value       = aws_cloudfront_distribution.cf_distribution.domain_name
+  value       = module.dev.cloudfront_domain_name
 }
 
 output "cloudfront_dist_id" {
   description = "Cloudfront distribution id"
-  value       = aws_cloudfront_distribution.cf_distribution.id
+  value       = module.dev.cloudfront_dist_id
 }
