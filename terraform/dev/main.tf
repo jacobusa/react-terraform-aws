cd3f9d2cc41ae6a174d53062810775af8b46e674
Kobus <jacobusbad@gmail.com>
2023-06-13 09:55:05 -0700

    add email dns validation maybe

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,6 +1,7 @@
 module "dev" {
   source             = "../static-web-module"
   domain_name        = "justneedadomain.xyz"
+  cloudfront_aliases = "dev.justneedadomain.xyz"
   use_default_domain = true
   region             = var.region
   bucket_name        = "sample-vite-terraform-aws"
