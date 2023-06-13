4189dacfb15bf5d1c55ae08a35979e3b2c74e3ab
Kobus <jacobusbad@gmail.com>
2023-06-13 08:50:15 -0700

    fix prod module outputs

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_id" {
   description = "Bucket ID for s3 holding out website files"
-  value       = module.dev.bucket_id
+  value       = module.prod.bucket_id
 }
 
 output "cloudfront_domain_name" {
   description = "Cloudfront domain name"
-  value       = module.dev.cloudfront_domain_name
+  value       = module.prod.cloudfront_domain_name
 }
 
 output "cloudfront_dist_id" {
   description = "Cloudfront distribution id"
-  value       = module.dev.cloudfront_dist_id
+  value       = module.prod.cloudfront_dist_id
 }
