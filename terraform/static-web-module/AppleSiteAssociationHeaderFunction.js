682a3ae5a5d9efb7f268d1dbaceef82e6a36b990
Kobus <jacobusbad@gmail.com>
2023-07-20 10:01:28 -0700

    more changes

--- terraform/static-web-module/AppleSiteAssociationHeaderFunction.js
+++ terraform/static-web-module/AppleSiteAssociationHeaderFunction.js
@@ -5,8 +5,9 @@ function handler(event) {
   if (uri === '/apple-app-site-association') {
     var response = {
       statusCode: 200,
+      statusDescription: 'OK',
       headers: {
-        'Content-Type': { value: 'application/json' },
+        'content-type': { value: 'application/json' },
       },
     };
     return response;
