12816a7ed96bfaddb8a8a548885263886021c998
Kobus <jacobusbad@gmail.com>
2024-05-27 11:47:38 -0700

    add build step

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -1,9 +1,12 @@
-import * as React from 'react'
-
+import * as React from 'react';
 
 export const Initial: React.FC = () => {
-    const mode = import.meta.env.MODE ?? ""
-    return (
-        <h1>Initial page. Bonjourno TERRAFORM WORKS. Mode: {mode} final product?</h1>
-        );
-}
+  const mode = import.meta.env.MODE ?? '';
+  const version = import.meta.env.VERSION ?? '';
+  return (
+    <h1>
+      Initial page. Bonjourno TERRAFORM WORKS. Mode: {mode} final product?
+      {version}
+    </h1>
+  );
+};
