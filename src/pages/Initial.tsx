6afa94b326c0a0d4ddf815c94b0ceb5b0c05d279
Kobus <jacobusbad@gmail.com>
2024-06-03 08:49:41 -0700

    add back terraform deploy

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 
 export const Initial: React.FC = () => {
   const mode = import.meta.env.MODE ?? '';
-  const version = import.meta.env.VERSION ?? '';
+  const version = import.meta.env.VITE_VERSION ?? '';
+  const fake = import.meta.env.VITE_FAKE ?? '';
   return (
     <h1>
       Initial page. Bonjourno TERRAFORM WORKS. Mode: {mode} final product?
-      {version}
+      version:{version}
+      <br />
+      mode:{mode}
+      <br />
+      fake:{fake}
     </h1>
   );
 };
